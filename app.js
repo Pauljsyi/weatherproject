c28dda18eaa8e83a73602962ba30cedb349aa51e
Paul Yi <pauljsyi@gmail.com>
2021-01-17 02:43:33 -0800

    utilize write method to response in order to display multiple html tags to render on webpage

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 
 
 app.get("/", function(req, res){
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Los%20Angeles&appid=a732389836363d79d1649abdaca4fdbb&units=imperial#';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=a732389836363d79d1649abdaca4fdbb&units=imperial#';
     
     //https get request over the internet to the url
     https.get(url , function(response){
@@ -18,10 +18,18 @@ app.get("/", function(req, res){
         response.on("data", function(data){
             const weatherData = JSON.parse(data)
             //logs data in hexadecimal format
-            console.log(weatherData.weather[0].description)
+            const location = weatherData.name
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            console.log(weatherData)
+            //res = refers to the app.get's callback function response param
+            
+            res.write("<h1>The temperature in " + location + " is " + temp + " degrees Fahrenheit.</h1>")
+            res.write("<p>The weather is currently " + weatherDescription + ".</p>")
+            res.send()
         })
     });
-res.send("server is up and running!")
+
 })
 
 app.listen(3000, function(){
