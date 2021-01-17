604445451bb02903e5ef574768be530d40513f17
Paul Yi <pauljsyi@gmail.com>
2021-01-17 02:09:26 -0800

    parse JSON data from API

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ app.get("/", function(req, res){
     //https get request over the internet to the url
     https.get(url , function(response){
         //logging the response from the api get request
-        console.log(response)
+        // console.log(response)
+        // console.log("statusCode: ", response.statusCode)
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data)
+            //logs data in hexadecimal format
+            console.log(weatherData.weather[0].description)
+        })
     });
 res.send("server is up and running!")
 })
@@ -20,3 +27,4 @@ res.send("server is up and running!")
 app.listen(3000, function(){
     console.log("server is running on port 3000")
 })
+
